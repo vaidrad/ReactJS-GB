8af533094662da72c49eaf664004f285b5e07783
Vaidrad <slava.serdyuk18@gmail.com>
2021-12-12 23:25:15 +0200

    lesson 1 homework done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Message } from './components/Message/Message'
+import { Button } from './components/Button/Button';
 
 function App() {
+  const messageText = 'Вот это вот пропа';
+  let yellow = false;
+
+  const handleButtonClick = () => {
+    yellow = !yellow;
+    console.log(yellow)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Message text={messageText} yellow={yellow}/>
+        <Button onButtonClick={handleButtonClick} title={'Изменить цвет'}/>
       </header>
     </div>
   );
