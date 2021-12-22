bcd5b572f6c219310300bcfd0518063383443b72
Vaidrad <slava.serdyuk18@gmail.com>
2021-12-22 23:29:13 +0200

    develop (*)

--- src/components/Button/Button.js
+++
@@ -1,8 +0,0 @@
-
-import React from "react";
-import './Button.css';
-
-
-export const Button = ({ title, onButtonClick }) => {
-  return <button className={'my-btn'} onClick={() => onButtonClick()}>{title}</button>;
-};
--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -3,6 +3,6 @@ import './Message.css';
 // import './Message.scss';
 // import './Message.less';
 
-export const Messages = ({ text, yellow }) => {
-    return <div className={`messageWrapper ${ yellow ? 'yellow' : 'false'}`}>{ text.map(({ author, messageBody }, index) => <p key={index}>{ author }: { messageBody }</p>) }</div>
+export const Messages = ({ text }) => {
+    return <div className={'messageWrapper yellow'}>{ text.map(({ author, messageBody, id }) => <p key={id}>{ author }: { messageBody }</p>) }</div>
 };
